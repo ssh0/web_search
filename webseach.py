2186cf4fcf9866e7bc7d9528aa39b92fecbbcec8
shotaro <shotaro@shotaro-Fujitsu@fuji.waseda.jp>
2014-09-21 19:20:30 +0900

    Committer: shotaro <shotaro@shotaro-Fujitsu@fuji.waseda.jp>
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   webseach.py
    	new file:   websearch/__init__.py
    	new file:   websearch/__init__.pyc
    	new file:   websearch/config.py
    	new file:   websearch/config.pyc
    	new file:   websearch/prediction.py
    	new file:   websearch/prediction.pyc

---
+++ webseach.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# written by Shotaro Fujimoto, September
+#
+# NOTE: bug: This code doesn't run correctly when firefox hasn't run.
+
+from gi.repository import Gtk
+import os
+import commands
+import sys
+
+import websearch.config as config
+import websearch.prediction as prediction
+logfile = config.logfile
+
+
+class Window(Gtk.Window):
+
+    def __init__(self):
+        self.window = Gtk.Window.__init__(self, title="search")
+
+        # list data for completion from logfile
+        lists = []
+        with open(logfile) as f:
+            for s in f.readlines():
+                lists += unicode(s, 'utf-8').split()[1:]
+        lists = set(lists)
+        liststore = Gtk.ListStore(str)
+        for match in lists:
+            liststore.append([match])
+
+        self.entry = prediction.EntryMultiCompletion()
+        self.entry.completion.set_model(liststore)
+        self.entry.completion.set_text_column(0)
+        self.entry.completion.set_popup_completion(popup_completion=True)
+
+#        self.entry = Gtk.Entry()
+#        self.entry.set_completion(self.completion)
+        self.entry.connect("activate", self.enter_callback)
+        self.add(self.entry)
+
+    def enter_callback(self, event):
+        # get text from entry widget
+        search_term = self.entry.get_text().split()
+
+        # if text is None, do nothing
+        if len(search_term) == 0:
+            return 0
+
+        # in config.py, site must be dictionary that
+        # key is option argument and value is website's address
+        site = config.site
+
+        # find option
+        option= search_term[0]
+        if option in site:
+            goto = site[option]
+            del search_term[0]
+            if len(search_term) == 0:
+                return 0
+        # if there are no option, go to default site
+        else:
+            goto = site['default-search']
+
+        # search term are joined mostly with '+'
+        if len(search_term) > 1:
+            t = '+'.join(search_term)
+        else:
+            t = search_term[0]
+
+        #print base + goto % t
+        # save the log to logfile
+        date = commands.getoutput('date +%F_%T')
+        log =  date + ' ' + t.replace('+', ' ') + '\n'
+        with open(logfile, 'a') as l:
+            l.writelines(log)
+
+        # goto website
+        base = config.browser['default']
+        os.system(base + goto % t)
+        sys.exit()
+
+
+def main():
+    win = Window()
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+
+if __name__ == '__main__':
+    main()
---
+++ websearch/config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# written by Shotaro Fujimoto, September 2014
+
+logfile = '/home/shotaro/Dropbox/log.txt'
+
+# choose the default browser by comment out
+# or you can edit to any browser
+browser = {'default':
+           'firefox -new-tab '
+#           'chromium-browser -new-tab '
+#           'google-chrome -new-tab '
+#           'opera -newtab '
+            ,
+           }
+
+site = {
+        # default: Google検索
+        'default-search': r'https://www.google.co.jp/#q=%s',
+
+        # 'w': Wikipedia
+        '-w': r'https:ja.wikipedia.org/wiki/%s',
+
+        # 'n': niconico動画
+        '-n': r'http://www.nicovideo.jp/search/%s',
+
+        # 'p': Google画像検索
+        #'-p': r'https://www.google.com/search?q=%s&um=1&ie=UTF-8&hl=ja&tbm=isch&source=og&sa=N&tab=wi',
+
+        # 'y': Youtubeで検索
+        '-y': r'http://www.youtube.com/results?search_query=%s&sm=3',
+
+        # 'rt' Yahooリアルタイム検索
+        '-rt': r'http://realtime.search.yahoo.co.jp/search?p=%s&ei=UTF-8',
+
+        # 'sc' Google Scholar検索
+        '-sc': r'http://scholar.google.co.jp/scholar?lr=&q=%s&hl=ja&as_sdt=0,5&as_vis=1',
+
+        # '-t': 翻訳
+        '-t': r'http://ejje.weblio.jp/content/%s'
+        }
