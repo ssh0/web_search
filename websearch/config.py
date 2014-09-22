8e1b17a284e4aaf72f78827d1ac0015948811f88
shotaro <shotaro@shotaro-Fujitsu@fuji.waseda.jp>
2014-09-22 15:32:59 +0900

    Changes to be committed: 	modified:   config.py 	modified:   config.pyc

--- websearch/config.py
+++ websearch/config.py
@@ -3,40 +3,40 @@
 #
 # written by Shotaro Fujimoto, September 2014
 
-logfile = '/home/shotaro/Dropbox/log.txt'
+logfile = "/home/shotaro/Dropbox/log.txt"
 
 # choose the default browser by comment out
 # or you can edit to any browser
-browser = {'default':
-           'firefox -new-tab '
-#           'chromium-browser -new-tab '
-#           'google-chrome -new-tab '
-#           'opera -newtab '
+browser = {"default":
+           "firefox -new-tab "
+#           "chromium-browser -new-tab "
+#           "google-chrome -new-tab "
+#           "opera -newtab "
             ,
            }
 
 site = {
         # default: Google検索
-        'default-search': r'https://www.google.co.jp/#q=%s',
+        "default-search": r"https://www.google.co.jp/#q=%s",
 
-        # 'w': Wikipedia
-        '-w': r'https:ja.wikipedia.org/wiki/%s',
+        # "w": Wikipedia
+        "-w": r"https:ja.wikipedia.org/wiki/%s",
 
-        # 'n': niconico動画
-        '-n': r'http://www.nicovideo.jp/search/%s',
+        # "n": niconico動画
+        "-n": r"http://www.nicovideo.jp/search/%s",
 
-        # 'p': Google画像検索
-        #'-p': r'https://www.google.com/search?q=%s&um=1&ie=UTF-8&hl=ja&tbm=isch&source=og&sa=N&tab=wi',
+        # "p": Google画像検索
+        #"-p": r"https://www.google.com/search?q=%s&um=1&ie=UTF-8&hl=ja&tbm=isch&source=og&sa=N&tab=wi",
 
-        # 'y': Youtubeで検索
-        '-y': r'http://www.youtube.com/results?search_query=%s&sm=3',
+        # "y": Youtubeで検索
+        "-y": r"http://www.youtube.com/results?search_query=%s&sm=3",
 
-        # 'rt' Yahooリアルタイム検索
-        '-rt': r'http://realtime.search.yahoo.co.jp/search?p=%s&ei=UTF-8',
+        # "rt" Yahooリアルタイム検索
+        "-rt": r"http://realtime.search.yahoo.co.jp/search?p=%s&ei=UTF-8",
 
-        # 'sc' Google Scholar検索
-        '-sc': r'http://scholar.google.co.jp/scholar?q=%s&hl=ja&as_sdt=0,5',
+        # "sc" Google Scholar検索
+        "-sc": r"http://scholar.google.co.jp/scholar?q=%s&hl=ja&as_sdt=0,5",
 
-        # '-t': 翻訳
-        '-t': r'http://ejje.weblio.jp/content/%s'
+        # "-t": 翻訳
+        "-t": r"http://ejje.weblio.jp/content/%s"
         }
